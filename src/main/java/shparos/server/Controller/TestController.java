a8703d76cf606ba5c6f47bdf3c415d9a6c8af3b5
DESKTOP-KA9I336\교육생42 <affogato128@gmail.com>
2023-10-18 12:10:02 +0900

    git action test2

--- src/main/java/shparos/server/Controller/TestController.java
+++ src/main/java/shparos/server/Controller/TestController.java
@@ -1,4 +1,15 @@
 package shparos.server.Controller;
 
-public class TestControllerg{
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class TestController{
+
+    @GetMapping("test")
+    public String test(){
+        return "this is test";
+    }
 }
